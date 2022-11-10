f7566df388f2d69c936fe525eb469867a6e2c7d4
theusdido <edilsonbitencourt@hotmail.com>
2022-11-09 21:51:54 -0300

    Não foi fácil, mas conseguimos.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Credencial } from '../crendecial';
 import { LocalStorageService } from '../local-storage.service';
 
 @Component({
@@ -7,25 +8,22 @@ import { LocalStorageService } from '../local-storage.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  public site:string  = '';
-  public login:string = '';
-  public senha:string = '';
+  public _credencial = {} as Credencial;
   constructor(
     public local_storage:LocalStorageService
   ) {
-    this.site   = this.local_storage.get('site');
-    this.login  = this.local_storage.get('login');
-    this.senha  = this.local_storage.get('senha');
+    this.carregar();
   }
-
+  carregar(){
+    //this.site   = this.local_storage.get('site');
+    //this.login  = this.local_storage.get('login');
+    //this.senha  = this.local_storage.get('senha');
+  }
   armazenar(){
-    this.local_storage.set('site',this.site);
-    this.local_storage.set('login',this.login);
-    this.local_storage.set('senha',this.senha);
+    this.local_storage.append('credenciais',this._credencial);
   }
   limpar(){
-    //this.local_storage.clear();
-    this.local_storage.del('senha');
-    //this.local_storage.rm('senha');
+    // Serviço que limpa os dados do local storage
+    this.local_storage.clear();
   }
 }
--- src/app/local-storage.service.ts
+++ src/app/local-storage.service.ts
@@ -8,7 +8,7 @@ export class LocalStorageService {
   constructor() { }
 
   set(key:string,value:any){
-    this.local_storage.setItem(key,value);
+    this.local_storage.setItem(key,JSON.stringify(value));
   }
 
   get(key:string){
@@ -25,5 +25,23 @@ export class LocalStorageService {
 
   rm(key:string){
     this.local_storage.removeItem(key)
+  }
+
+  append(key:string,value:any){
+    // Retorna o valor que está no localStorage
+    let old_value = this.get(key) ;
+    let new_value:Array<any>;
+    if (old_value == null){
+      new_value = [];
+    }else{
+      // Transforma o valor de String para JSON
+      new_value  = JSON.parse(old_value);
+    }
+
+    // Adicionar o novo valor no vetor
+    new_value.push(value);
+
+    // Salva a nova informação no localStorage
+    this.set(key,new_value);
   }
 }
